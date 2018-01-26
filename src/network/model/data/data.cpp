5a0df1e648ed6eaa9427443cfe0a6bb72e267489
palatin <igorshamin96@gmail.com>
2018-01-26 13:27:24 +0200

    +Add documentation to Data class, change dataLength from double to unsigned long

--- src/network/model/data/data.cpp
+++ src/network/model/data/data.cpp
@@ -1,25 +1,7 @@
-#include <cstring>
 #include "data.h"
 
 
-
-/*wsn::model::Data::Data(wsn::model::Node *sender, wsn::model::Node *receiver, const unsigned char *const data, size_t dataLength) :sender(sender), receiver(receiver) {
-    this->length = dataLength;
-    this->data = new unsigned char[length];
-    memcpy(this->data, data, sizeof(unsigned char) * dataLength);
-}
-
-wsn::model::Data::Data(const wsn::model::Data &data) :sender(data.sender), receiver(data.receiver) {
-    this->length = data.length;
-    this->data = new unsigned char[data.length];
-    memcpy(this->data, data.data, sizeof(unsigned char) * data.length);
-}
-
-wsn::model::Data::~Data()  {
-    delete[] this->data;
-}*/
-
-wsn::model::Data::Data(const wsn::model::Node *sender, const wsn::model::Node *receiver, double dataLength) :sender(sender), receiver(receiver), length(dataLength) {
+wsn::model::Data::Data(const wsn::model::Node *sender, const wsn::model::Node *receiver, unsigned long dataLength) :sender(sender), receiver(receiver), length(dataLength) {
 }
 
 double wsn::model::Data::getDataLength() const {
