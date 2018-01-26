1fc1d908b486540af19545a420353356e96cb72c
palatin <igorshamin96@gmail.com>
2018-01-26 13:31:36 +0200

    +Change network protocol to template type to properly work with different physics controllers

--- src/network/protocol/network_protocol.h
+++ src/network/protocol/network_protocol.h
@@ -6,8 +6,11 @@
 
 #include <vector>
 #include <queue>
+#include <memory>
+
 #include "../model/data/data.h"
 #include "../model/data/command.h"
+#include "../controller/physics/physics_controller.h"
 
 namespace wsn {
 
@@ -25,31 +28,44 @@ namespace wsn {
             using model::Data;
             using model::Node;
 
-            //class Data;
-
+            template <typename NodeType>
             class NetworkProtocol {
 
             public:
-                NetworkProtocol();
+
+                explicit NetworkProtocol(std::shared_ptr<wsn::controller::physics::PhysicsController<NodeType>> physicsController) : physicsControllerPtr(std::move(physicsController)) {}
+                virtual ~NetworkProtocol() = default;
 
                 virtual void step(long time) = 0;
 
 
             protected:
-                bool sendData(const Data &data, const Node *receiver) const;
-                const std::vector<wsn::model::Command>& getCommands(const Node *node);
-                const std::deque<wsn::model::Data>& getData(const Node *node);
+                bool sendData(const Data &data, NodeType &sender, NodeType &receiver) const {
+                    return this->physicsControllerPtr.get()->sendData(data, sender, receiver);
+                }
 
-                //virtual void test(const model::Node &node) = 0;
+                bool receiveData(const Data &data, NodeType &sender, NodeType &receiver) const {
+                    return this->physicsControllerPtr.get()->receiveData(data, receiver);
+                }
+
+                const std::vector<wsn::model::Command>& getCommands(const Node *node) const {
+                    return node->getCommands();
+                }
+
+                const std::deque<wsn::model::Data>& getData(const Node *node) const {
+                    return node->getData();
+                }
+
 
             private:
-                //bool receiveData(const Data &data) const;
-                //bool processData(const Data &data) const;
+                const std::shared_ptr<wsn::controller::physics::PhysicsController<NodeType>> physicsControllerPtr;
             };
+
 
         }
     }
 }
+
 
 
 #endif //WSN_NETWORK_PROTOCOL_H
