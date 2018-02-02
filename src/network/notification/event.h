121f535176b14a864272c34735a9c8ba13522ffc
palatin <igorshamin96@gmail.com>
2018-02-02 17:08:29 +0200

    +Add notification system

---
+++ src/network/notification/event.h
@@ -0,0 +1,15 @@
+
+
+#ifndef WSN_EVENT_H
+#define WSN_EVENT_H
+
+namespace wsn {
+
+    namespace notification {
+
+        enum Event {
+            NodeDeath
+        };
+    }
+}
+#endif //WSN_EVENT_H
