7ec9950d0d2d8ca8c153ebd57af62c8beec97113
palatin <igorshamin96@gmail.com>
2018-02-06 21:02:47 +0200

    implement wsn_controller

--- src/network/controller/wsn_controller.cpp
+++ src/network/controller/wsn_controller.cpp
@@ -1,8 +1,45 @@
-//
-// Created by Игорь on 31.12.2017.
-//
+
 
 #include "wsn_controller.h"
+
+
+void wsn::controller::WSNController::addComponent(wsn::controller::WSNComponentPtr component) {
+    stop();
+    wsnComponents.push_back(std::move(component));
+    start();
+}
+
+void wsn::controller::WSNController::start() {
+    if(isRunning == false) {
+        this->updateThread = std::thread(&WSNController::update, this);
+        isRunning = true;
+    }
+}
+
+void wsn::controller::WSNController::stop() {
+    isRunning = false;
+    updateThread.join();
+}
+
+void wsn::controller::WSNController::setUpdateSpeed(float speed) {
+    this->updateSpeed = speed;
+}
+
+
+
+void wsn::controller::WSNController::update() {
+
+    while(isRunning) {
+
+        for (auto &component : wsnComponents) {
+            component->update();
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(updateSpeed));
+    }
+
+}
+
 
 
 
---
+++ src/network/core/wsn_component.h
@@ -0,0 +1,20 @@
+//
+// Created by palatin on 2/5/18.
+//
+
+#ifndef WSN_WSNCOMPONENT_H
+#define WSN_WSNCOMPONENT_H
+
+namespace wsn {
+
+    namespace core {
+
+        class WSNComponent {
+
+        public:
+            virtual void update() = 0;
+
+        };
+    }
+}
+#endif //WSN_WSNCOMPONENT_H
